3b32100a3f7cab581c7505ade8f05f22be35d29c
AlphaSquad <adrifree@googlemail.com>
2019-05-02 11:25:01 +0200

    Removed flow fixing and filter smaller graphs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main (int argc, char* argv[])
 	clock_t t = clock();
 	clock_t t_start = clock();
 	std::cerr << "Building deBruijnGraph..." << std::endl;
-	//deBruijnGraph dbg = deBruijnGraph(std::string(argv[1]), atoi(argv[2])); // read file + k
+	//deBruijnGraph *dbg = new deBruijnGraph(std::string(argv[1]), atoi(argv[2])); // read file + k
 	deBruijnGraph *dbg = new deBruijnGraph(std::string(argv[1])); // dump file
     std::cerr << "Building deBruijnGraph took " << (clock() - t)/1000000. << " seconds." << std::endl;
     t = clock();
@@ -25,11 +25,18 @@ int main (int argc, char* argv[])
     unsigned int i = 0;
     for (auto&& d : dbgs)
     {
-	    UnitigGraph ug(*d, atof(argv[2]));
+        if (d->getSize() < 500) // ignore small graphs
+        {
+            std::cerr << "Skipping small dBg" << std::endl;
+            continue;
+	    }
+        UnitigGraph ug(*d, atof(argv[2]));
+	    //UnitigGraph ug(*d, atof(argv[3]);
         delete d;
         //t = clock();
         //ug.debug();
         ug.assemble(argv[3] + std::to_string(i++));
+        //ug.assemble(argv[4] + std::to_string(i++));
     }
 	std::cerr << "The complete assembly process took " << (clock() - t_start)/1000000. << " seconds." << std::endl;
     return 0;
